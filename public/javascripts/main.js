b8e4d1be18c26100e29813cd8a8073f46c6f32d4
JJSolvang <jacobsolvang@gmail.com>
2022-03-15 21:25:47 -0700

    Updated to store and request data from node (storage is temporary)

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,21 +9,17 @@ let NoteObject = function (pTitle, pYear, pType) {
     this.type = pType;
 }
 
-function Start() {
-noteArray.push(new NoteObject("Destiny", "2014", "FPS"));
-noteArray.push(new NoteObject("Destiny 2", "2017", "FPS"));
-noteArray.push(new NoteObject("Portal 2", "2011", "FPS"));
-console.log(noteArray);
-}
-
 document.addEventListener("DOMContentLoaded", function (event) {
 
     document.getElementById("buttonAdd").addEventListener("click", function () {
-
-        noteArray.push(new NoteObject(document.getElementById("title").value, document.getElementById("year").value, selectedType));
-        console.log(noteArray);
-        document.getElementById("title").value = "";
-        document.getElementById("year").value = "";
+        $.get("/getSelectedType", function(data, status) {
+            selectedType = data;
+            
+            noteArray.push(new NoteObject(document.getElementById("title").value, document.getElementById("year").value, selectedType));
+            console.log(noteArray);
+            document.getElementById("title").value = "";
+            document.getElementById("year").value = "";
+        });
     });
 
     $(document).bind("change", "#select-type", function (event, ui) {
@@ -42,41 +38,42 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 
 function createList() {
+
+    $.get("/getAllGames", function(data, status) {
+        noteArray = data;
+        var myul = document.getElementById("myList");
+        myul.innerHTML = '';
     
-    // clear prior data
-
-
-    var myul = document.getElementById("myList");
-    myul.innerHTML = '';
-
-    noteArray.forEach(function (element, i) {   // use handy array forEach method
-        var li = document.createElement('li');
-        li.classList.add('oneGame');
-        li.innerHTML = element.title + ":  " + element.year + "  " + element.type;
-        myul.appendChild(li);
-    });
-
-    var liList = document.getElementsByClassName('oneGame')
-    console.log(liList);
-
-    let newGameArray = Array.from(liList);
-    newGameArray.forEach(function (element, i) {
-        element.addEventListener('click', function () {
-            favNoteArray.push(noteArray[i]);
+        noteArray.forEach(function (element, i) {   // use handy array forEach method
+            var li = document.createElement('li');
+            li.classList.add('oneGame');
+            li.innerHTML = element.title + ":  " + element.year + "  " + element.type;
+            myul.appendChild(li);
+        });
+    
+        var liList = document.getElementsByClassName('oneGame')
+        console.log(liList);
+    
+        let newGameArray = Array.from(liList);
+        newGameArray.forEach(function (element, i) {
+            element.addEventListener('click', function () {
+                favNoteArray.push(noteArray[i]);
+            });
         });
     });
 };
 
 function createList2() {
+    $.get("/getAllFavGames", function(data, status) {
+        favNoteArray = data;
+
+        var myul2 = document.getElementById("myFavoriteList");
+        myul2.innerHTML = '';
     
-    // clear prior data
-
-    var myul2 = document.getElementById("myFavoriteList");
-    myul2.innerHTML = '';
-
-    favNoteArray.forEach(function (element, i) {   // use handy array forEach method
-        var li2 = document.createElement('li');
-        li2.innerHTML = element.title + ":  " + element.year + "  " + element.type;
-        myul2.appendChild(li2);
+        favNoteArray.forEach(function (element, i) {   // use handy array forEach method
+            var li2 = document.createElement('li');
+            li2.innerHTML = element.title + ":  " + element.year + "  " + element.type;
+            myul2.appendChild(li2);
+        });
     });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile('index.html');
-});
-
-/* GET list page. */
-router.get('/#list', function(req, res) {
-  res.render('#list');
-});
-
-let noteArray = [];
+let serverNoteArray = [];
+let serverFavNoteArray = [];
 let selectedType = "";
 
 // define a constructor to create note objects
@@ -21,11 +12,28 @@ let NoteObject = function (pTitle, pYear, pType) {
     this.type = pType;
 }
 
-function Start() {
-    noteArray.push(new NoteObject("Destiny", "2014", "FPS"));
-    noteArray.push(new NoteObject("Destiny 2", "2017", "FPS"));
-    noteArray.push(new NoteObject("Portal 2", "2011", "FPS"));
-    console.log(noteArray);
-}
+serverNoteArray.push(new NoteObject("Destiny", "2014", "FPS"));
+serverNoteArray.push(new NoteObject("Destiny 2", "2017", "FPS"));
+serverNoteArray.push(new NoteObject("Portal 2", "2011", "FPS"));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('index.html');
+});
+
+// update game data
+router.get("/getSelectedType", function(req, res) {
+  res.status(200).json(selectedType)
+});
+
+// update game data
+router.get("/getAllGames", function(req, res) {
+  res.status(200).json(serverNoteArray)
+});
+
+// update game data
+router.get("/getAllFavGames", function(req, res) {
+  res.status(200).json(serverFavNoteArray)
+});
 
 module.exports = router;
